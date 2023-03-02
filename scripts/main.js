c4ceb8ddabae81704987a1dbdddd5d5562245123
cooper <cooper.elliott30@gmail.com>
2023-03-02 14:35:50 +1100

    Add mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+/* 
+Helpful Links:
+https://www.youtube.com/watch?v=hBbrGFCszU4
+https://www.youtube.com/watch?v=flItyHiDm7E
+ */
+
 // const dropdowns = document.querySelectorAll('ul ul');
 
 const dropdowns = document.querySelectorAll('.dropdown');
@@ -7,20 +13,81 @@ console.log(dropdowns);
 
 dropdowns.forEach((dropdown) => {
   dropdown.addEventListener('click', displayDropdown);
-  dropdown.addEventListener('mouseover', displayDropdown);
+  dropdown.addEventListener('mouseover', mouseoverDropdown);
 
   function displayDropdown() {
+    console.log('click');
     const ul = dropdown.querySelector('ul');
-    ul.classList.toggle('hidden');
+    console.log(ul.classList);
+    if (ul.classList.contains('hidden')) {
+      ul.classList.remove('hidden');
+    } else {
+      ul.classList.add('hidden');
+    }
+  }
+
+  function mouseoverDropdown() {
+    console.log('mouseover');
+    const ul = dropdown.querySelector('ul');
+    console.log(ul.classList);
+    if (ul.classList.contains('hidden')) {
+      ul.classList.remove('hidden');
+    }
   }
 
   dropdown.addEventListener('mouseout', hideDropdown);
 
   function hideDropdown() {
+    console.log('mouseout');
     const ul = dropdown.querySelector('ul');
     ul.classList.add('hidden');
   }
 });
+
+// add mobile logic
+const mobileMenu = document.querySelector('.mobile-menu');
+
+mobileMenu.addEventListener('click', showNav);
+
+function showNav() {
+  const nav = document.querySelector('.nav-bar');
+  const ul = nav.querySelector('ul');
+
+  if (!ul.classList.contains('active')) {
+    ul.classList.add('active');
+    mobileMenu.classList.add('active');
+  } else {
+    ul.classList.remove('active');
+    mobileMenu.classList.remove('active');
+  }
+
+  // if (window.getComputedStyle(ul).display === 'none') {
+  //   mobileMenu.classList.add('active');
+  //   console.log('display is none');
+  //   ul.style.display = 'initial';
+  // } else {
+  //   mobileMenu.classList.remove('active');
+  //   console.log('display isnt none');
+  //   ul.style.display = 'none';
+  // }
+}
+
+/* change display with JS - alternative to adding class
+
+function showNav() {
+  const nav = document.querySelector('.nav-bar');
+  const ul = nav.querySelector('ul');
+  if (window.getComputedStyle(ul).display === 'none') {
+    mobileMenu.classList.add('active');
+    console.log('display is none');
+    ul.style.display = 'initial';
+  } else {
+    mobileMenu.classList.remove('active');
+    console.log('display isnt none');
+    ul.style.display = 'none';
+  }
+}
+*/
 
 // for (let i = 0; i < dropdowns.length; i++) {
 //   dropdowns[i].parentElement.addEventListener('click', displayDropdown);
